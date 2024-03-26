e99cf8c9630a96076ab0c89718dc7209135dfba2
Urufusan <63711492+Urufusan@users.noreply.github.com>
2024-03-26 23:04:35 +0100

    I think this works don't blame me

--- RUNME.py
+++ RUNME.py
@@ -8,20 +8,25 @@ if __name__ == "__main__":
     #TODO: Make a nice little automated thing for starting the main files, list them out in a json file and the files are automatically found and ran, PWD set ofc.
     command1 = """sh -c "cd src/server/ && python3 web_server_main.py" """.strip()
     command2 = """sh -c "cd src/aggregator/ && python3 discord_aggregator.py" """.strip()
+    command3 = """sh -c "cd src/chat_integration && python3 chat_effects_obs.py" """.strip()
 
     # Create two processes for each command
     process1 = multiprocessing.Process(target=run_command, args=(command1,))
     process2 = multiprocessing.Process(target=run_command, args=(command2,))
-
+    process3 = multiprocessing.Process(target=run_command, args=(command3,))
+    
     # Start both processes
     process1.start()
     process2.start()
+    process3.start()
 
     try:
         # Keep the script running until interrupted
         process1.join()
         process2.join()
+        process3.join()
     except KeyboardInterrupt:
         # If interrupted, terminate both processes
         process1.terminate()
         process2.terminate()
+        process3.terminate()
--- src/chat_integration/chat_effects_obs.py
+++ src/chat_integration/chat_effects_obs.py
@@ -70,60 +70,80 @@ if __name__ == "__main__":
     #     except KeyboardInterrupt:
     #         break
     #     time.sleep(0.2)
-    while True:
-        _split_message_contents: list[str] = input(">> ").split(" ")
-        if len(_split_message_contents) <= 1:
-            print("fail!")
-            continue
-        try:
-            match _split_message_contents[1]:
-                case "skew":
-                    cl.set_source_filter_settings("facecam", "3D Effect", {
-                                            'rot_x': float(_split_message_contents[2]),
-                                            'rot_y': float(_split_message_contents[3]),
-                                            'rot_z': float(_split_message_contents[4])},
-                                            overlay=True)
+    # while True:
+    #     _split_message_contents: list[str] = input(">> ").split(" ")
+    #     if len(_split_message_contents) <= 1:
+    #         print("fail!")
+    #         continue
+    #     try:
+    #         match _split_message_contents[1]:
+    #             case "skew":
+    #                 cl.set_source_filter_settings("facecam", "3D Effect", {
+    #                                         'rot_x': float(_split_message_contents[2]),
+    #                                         'rot_y': float(_split_message_contents[3]),
+    #                                         'rot_z': float(_split_message_contents[4])},
+    #                                         overlay=True)
 
-                case _chat_command_str if _chat_command_str in ['brightness', 'contrast', 'gamma', 'hue', 'saturation']:
-                    print({("hue_shift" if _chat_command_str == "hue" else _chat_command_str): _split_message_contents[2]})
-                    cl.set_source_filter_settings("facecam", "Color Correction", {
-                    ("hue_shift" if _chat_command_str == "hue" else _chat_command_str): float(_split_message_contents[2])}, overlay=True)
+    #             case _chat_command_str if _chat_command_str in ['brightness', 'contrast', 'gamma', 'hue', 'saturation']:
+    #                 print({("hue_shift" if _chat_command_str == "hue" else _chat_command_str): _split_message_contents[2]})
+    #                 cl.set_source_filter_settings("facecam", "Color Correction", {
+    #                 ("hue_shift" if _chat_command_str == "hue" else _chat_command_str): float(_split_message_contents[2])}, overlay=True)
                 
-                case "exit":
+    #             case "exit":
+    #                 break
+
+    #             case _:
+    #                 print("fail!")
+
+    #     except KeyboardInterrupt:
+    #         break
+
+    #     except Exception as e:
+    #         print(e)
+    
+    # cl.disconnect()
+
+    # exit(0)
+    last_used_skew = time.time()
+    last_used_cc = time.time()
+    
+    chat = pytchat.create(video_id=get_stream_id())
+    while chat.is_alive():
+        for c in chat.get().sync_items():
+            print(f"{c.datetime} [{c.author.name}]- {c.message}")
+            if c.message.startswith("!cam"):
+                _split_message_contents: list[str] = c.message.split(" ")
+                if len(_split_message_contents) <= 1:
+                    print("fail!")
+                    continue
+                try:
+                    match _split_message_contents[1]:
+                        case "skew":
+                            if time.time() - last_used_skew > 15:
+                                cl.set_source_filter_settings("facecam", "3D Effect", {
+                                                        'rot_x': float(_split_message_contents[2]),
+                                                        'rot_y': float(_split_message_contents[3]),
+                                                        'rot_z': float(_split_message_contents[4])},
+                                                        overlay=True)
+                                last_used_skew = time.time()
+
+                        case _chat_command_str if _chat_command_str in ['brightness', 'contrast', 'gamma', 'hue', 'saturation']:
+                            if time.time() - last_used_cc > 5:
+                                print({("hue_shift" if _chat_command_str == "hue" else _chat_command_str): _split_message_contents[2]})
+                                cl.set_source_filter_settings("facecam", "Color Correction", {
+                                ("hue_shift" if _chat_command_str == "hue" else _chat_command_str): float(_split_message_contents[2])}, overlay=True)
+                                last_used_cc = time.time()
+
+                        # case "exit":
+                        #     break
+
+                        case _:
+                            print("fail!")
+
+                except KeyboardInterrupt:
                     break
 
-                case _:
-                    print("fail!")
-
-        except KeyboardInterrupt:
-            break
-
-        except Exception as e:
-            print(e)
-    cl.disconnect()
-
-# exit(0)
-# chat = pytchat.create(video_id=get_stream_id())
-# while chat.is_alive():
-#     for c in chat.get().sync_items():
-#         print(f"{c.datetime} [{c.author.name}]- {c.message}")
-#         if c.message.startswith("!cam"):
-#             _split_message_contents: list[str] = c.message.split(" ")
-#             if not len(_split_message_contents) > 1:
-#                 continue
-#             try:
-#                 match _split_message_contents[1]:
-#                     case "skew":
-#                         cl.set_source_filter_settings("facecam", "3D Effect", {
-#                                                 'rot_x': _split_message_contents[2],
-#                                                 'rot_y': _split_message_contents[3],
-#                                                 'rot_z': _split_message_contents[4]},
-#                                                 overlay=True)
-#                     case _chat_command_str if _chat_command_str in ['brightness', 'contrast', 'gamma', 'hue', 'opacity', 'saturation']:
-#                         cl.set_source_filter_settings("facecam", "Color Correction", {
-#                     ("hue_shift" if _chat_command_str == "hue" else _chat_command_str): _split_message_contents[2]},
-#                             overlay=True)
-#             except Exception as e:
-#                 print(e)
-            # e.g. !cam gamma 1.0
-            # e.g. !cam 90 90 90 (skewing in 3D)
+                except Exception as e:
+                    print(e)
+                # e.g. !cam gamma 1.0
+                # e.g. !cam 90 90 90 (skewing in 3D)
