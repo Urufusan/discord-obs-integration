55c5d1e15f75cfbcc2d7885331b346c4707ac67a
Urufusan <63711492+Urufusan@users.noreply.github.com>
2024-03-29 21:35:36 +0100

    i hate that i have to do this

--- src/aggregator/discord_aggregator.py
+++ src/aggregator/discord_aggregator.py
@@ -225,7 +225,9 @@ async def camera_control(ctx: commands.Context[commands.Bot], cmd_name: str, *cm
     global last_used_cc
     
     # this is utter madness
-    cmd_args = ctx.message.content.lstrip("!cam ").lstrip("!cm ").split(" ").pop(cmd_name)
+    ze_command = ctx.message.content.lstrip("!cam ").lstrip("!cm ").split(" ")
+    ze_command.pop(cmd_name)
+    cmd_args = ze_command
     # FIX THIS
     # cmd_args = [_f_c_a[0] for _f_c_a in cmd_args]
     print("cam cmd!", obs_cl, (ctx.message.channel.id in discord_obs_control_channels))
