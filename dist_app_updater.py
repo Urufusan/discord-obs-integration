c7c871d904612afccf349c7dd4f8a2e2a665cd0a
Urufusan <63711492+Urufusan@users.noreply.github.com>
2024-04-22 19:42:53 +0200

    Missing discord, oops!

--- dist_app_updater.py
+++ dist_app_updater.py
@@ -53,7 +53,7 @@ def move_files_from_directory():
     
     # Get a list of files in the source directory
     os.system(f"cp -aR {source_dir}/* .")
-    os.system(f"{'pip3' if platform.system() != 'Windows' else 'py -m pip'} install flask flask-sock requests httpx httpx[http2] websocket-client")
+    os.system(f"{'pip3' if platform.system() != 'Windows' else 'py -m pip'} install flask flask-sock discord requests httpx httpx[http2] websocket-client")
     # Remove the source directory
     try:
         shutil.rmtree(source_dir)
